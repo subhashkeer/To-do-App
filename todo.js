74c8032d4e566fe79ece39ff9b86d8bc717f6e27
subhashkeer@gmail.com <subhashkeer@gmail.com>
2018-10-02 13:34:34 +0530

    To do App

---
+++ todo.js
@@ -0,0 +1,68 @@
+var ul = document.getElementById('list');
+var li;
+
+
+var addButton = document.getElementById('add');
+addButton.addEventListener('click', addItem);            
+
+var removeButton = document.getElementById('remove');
+removeButton.addEventListener('click', removeItem);
+
+var removeAllButton = document.getElementById('removeall');
+removeAllButton.addEventListener('click', removeall);
+
+function addItem(){
+   var input = document.getElementById('input');
+   var item = input.value;
+   ul = document.getElementById('list');
+   var textnode = document.createTextNode(item);
+
+  // if( item === " " || item === null)
+  if(item.length == 0)
+  {
+         alert("Please Add Todo Item");
+       return false;
+   }
+   else{
+     // Create li   
+       li = document.createElement('li');
+     // Create checkbox  
+       var checkbox = document.createElement('input');
+       checkbox.type = 'checkbox';
+       checkbox.setAttribute('id','check');
+     // Create label
+     var label = document.createElement('label');
+     label.setAttribute('for', 'item'); // optional
+
+     //add element in HTML page
+
+     ul.appendChild(label);
+     li.appendChild(checkbox);
+     label.appendChild(textnode);
+     li.appendChild(label);
+     ul.insertBefore(li, ul.childNodes[0]);
+     setTimeout(() => {
+        li.className = 'visual';
+     }, 3)
+
+     input.value = '';
+ 
+   }
+
+}
+
+function removeItem(){
+    li = ul.children;
+    for(let index = 0; index < li.length; index++){
+        while(li[index] && li[index].children[0].checked){
+            ul.removeChild(li[index])
+        }
+    }
+}
+
+function removeall(){
+    li = ul.children;
+    for(let i = 0; i < li.length; i++){
+        ul.remove(li[i]);
+    }
+}
